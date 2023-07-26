867860cf7e370aff362196434188ba11a4f838ae
robertors <roberto@rssolucoesweb.com.br>
2023-07-26 08:43:04 -0300

    lista-formularios-e-ajuestes

--- src/routes/listaFormulariosProfessor.ts
+++ src/routes/listaFormulariosProfessor.ts
@@ -28,7 +28,6 @@ export async function listaFormulariosProfessor(app: FastifyInstance) {
               id: true,
               nome: true,
               tipo:true,
-              
             }
           }
         }
