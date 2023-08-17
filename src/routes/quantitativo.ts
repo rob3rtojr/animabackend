ce308fa89ab71f75c3129ee8df2251937d63c15d
robertors <roberto@rssolucoesweb.com.br>
2023-08-17 11:31:24 -0300

    Agrupar-por-escola

--- src/routes/quantitativo.ts
+++ src/routes/quantitativo.ts
@@ -15,12 +15,14 @@ export async function quantitativo(app: FastifyInstance) {
       municipioId: z.coerce.number().optional(),
       escolaId: z.coerce.number().optional(),
       turmaId: z.coerce.number().optional(),
+      agrupador: z.string().optional(),
     })    
 
     const { formularioId } = paramsSchema.parse(request.params)
-    const { estadoId, regionalId, municipioId, escolaId, turmaId } = querySchema.parse(request.query)
+    const { estadoId, regionalId, municipioId, escolaId, turmaId, agrupador } = querySchema.parse(request.query)
 
     let sql: string = ""
+
 
     //consulta o tipo do formulário
     const formulario = await prisma.formulario.findUnique({
@@ -39,15 +41,15 @@ export async function quantitativo(app: FastifyInstance) {
     let resultadoAtualizado
 
     if (formulario?.tipo === "aluno") {
-      resultado = await prisma.$queryRaw `exec SP_RelatorioAluno ${formularioId},${estadoId},${regionalId},${municipioId},${escolaId},${turmaId}`
+      resultado = await prisma.$queryRaw `exec SP_RelatorioAluno ${formularioId},${estadoId===undefined ? 0 : estadoId},${regionalId === undefined ? 0 : regionalId},${municipioId === undefined ? 0 : municipioId},${escolaId === undefined ? 0 : escolaId},${turmaId === undefined ? 0 : turmaId},${agrupador}`
       if (turmaId)
         resultadoAtualizado = atualizarNomes(resultado)
     }else {
-      resultado = await prisma.$queryRaw `exec SP_RelatorioProfessor ${formularioId},${estadoId},${regionalId},${municipioId}`
+      resultado = await prisma.$queryRaw `exec SP_RelatorioProfessor ${formularioId},${estadoId},${regionalId},${municipioId},${agrupador}`
       if (municipioId)
         resultadoAtualizado = atualizarNomes(resultado)
     }
-
+    //return `exec SP_RelatorioAluno ${formularioId},${estadoId===undefined ? 0 : estadoId},${regionalId === undefined ? 0 : regionalId},${municipioId === undefined ? 0 : municipioId},${escolaId === undefined ? 0 : escolaId},${turmaId === undefined ? 0 : turmaId},'${agrupador}'`
     return resultadoAtualizado ? resultadoAtualizado : resultado
   })
 }
