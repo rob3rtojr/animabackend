738dc48490a1b6277d1dcee3f5b35fe85ad6dbd2
robertors <roberto@rssolucoesweb.com.br>
2024-11-11 20:02:42 -0300

    permite formulario sem autenticacao

--- src/routes/formularioSA.ts
+++ src/routes/formularioSA.ts
@@ -21,6 +21,9 @@ export async function formularioSA(app: FastifyInstance) {
       ],
       where: {
         formularioId: id,
+        formulario: {
+          permiteSemAutenticacao: '1',
+        },
       },
       include: {
         alternativa: {
