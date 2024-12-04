6bbe61b0cc9c7962d1267f5507c1eefc67302351
robertors <roberto@rssolucoesweb.com.br>
2024-12-04 14:49:03 -0300

    quantitativo sa

--- src/routes/quantitativo.ts
+++ src/routes/quantitativo.ts
@@ -87,14 +87,15 @@ export async function quantitativo(app: FastifyInstance) {
     const result: QueryResult[] = await prisma.$queryRaw<QueryResult[]>`
       SELECT 
         e.id, e.sigla, e.nome,
-        CONVERT(VARCHAR(10), a.createdAt, 103) AS createdAt, 
+        FORMAT(a.createdAt, 'dd/MM/yyyy') AS createdAt, 
         COUNT(DISTINCT a.id) AS count
       FROM alunosa a
       INNER JOIN estado e ON a.estadoId = e.id
       INNER JOIN respostaalunosa r ON r.alunoSAId = a.id
       INNER JOIN pergunta p ON r.perguntaId = p.id
       WHERE p.formularioId = ${formularioId}
-      GROUP BY e.id, e.sigla, e.nome, CONVERT(VARCHAR(10), a.createdAt, 103)
+      GROUP BY e.id, e.sigla, e.nome, FORMAT(a.createdAt, 'dd/MM/yyyy')
+      order by FORMAT(a.createdAt, 'dd/MM/yyyy')
     `
 
     const formattedResult: FormattedResult[] = result.reduce(
